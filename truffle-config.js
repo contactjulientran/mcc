bd0472ff966d795ebf50d1094d4fd0470bcc0748
Oudom Suos <oudom.suos@gmail.com>
2021-04-08 11:11:33 +0200

    Ajout de la configuration truffle pour déployer sur ganache, ropsten et kovan, et des lib dotenv et @truffle/hdwallet-provider

--- truffle-config.js
+++ truffle-config.js
@@ -1,12 +1,34 @@
 const path = require("path");
+const HDWalletProvider = require('@truffle/hdwallet-provider');
+require('dotenv').config();
 
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // to customize your Truffle configuration!
   contracts_build_directory: path.join(__dirname, "client/src/contracts"),
   networks: {
-    develop: {
-      port: 8545
+    ganache: {
+      host: "localhost",
+      port: 7545,
+      network_id: 5777,
+    },
+    ropsten: {
+      provider: function () {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://ropsten.infura.io/v3/${process.env.INFURA_ID}`)
+      },
+      network_id: 3,
+      //from: "" adr à partir de laquelle le contrat va être déployé
+    },
+    kovan: {
+      provider: function () {
+        return new HDWalletProvider(`${process.env.MNEMONIC}`, `https://kovan.infura.io/v3/${process.env.INFURA_ID}`)
+      },
+      network_id: 42,
+    }
+  },
+  compilers: {
+    solc: {
+      version: "0.6.11",    // Fetch exact version from solc-bin (default: truffle's version)
     }
   }
 };
